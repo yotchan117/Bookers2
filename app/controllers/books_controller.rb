a99f35dc194f652d21dd26b62e5f06fee3be5508
EC2 Default User <ec2-user@ip-172-31-6-20.ap-northeast-1.compute.internal>
2022-10-14 09:34:53 +0000

    [Finish] テスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,13 +30,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    is_matching_login_user
     if @book.update(book_params)
       redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render :edit
     end
-
   end
 
   def destroy
