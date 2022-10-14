0cee467b02528ab8cfac49016f1d77783d40c02e
EC2 Default User <ec2-user@ip-172-31-6-20.ap-northeast-1.compute.internal>
2022-10-14 08:33:10 +0000

    [Update] 他人のユーザー情報編集画面に遷移できない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    
     @book = Book.find(params[:id])
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @users = User.all
     @user = User.find(current_user.id)
@@ -22,12 +24,19 @@ class UsersController < ApplicationController
     else
       render :edit
     end
-    
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if (user_id != login_user_id)
+      redirect_to user_path(current_user.id)
+    end
   end
 end
